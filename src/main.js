71c93e860f9c44bb3d8733a5cde976f307b3d74f
davitlabadze <labadzedato18@gmail.com>
2022-04-22 18:17:53 +0400

    navigation guards

--- src/main.js
+++ src/main.js
@@ -40,6 +40,18 @@ const router = createRouter({
   }
 });
 
+router.beforeEach(function(to,from,next){
+  console.log('Global beforeEach');
+  console.log(to, from);
+  // if(to.name === 'team-members') {
+  //   next();
+  // }else{
+  //   next({name: 'team-members', params: {teamId: 't2'}});
+  // }
+  next();
+
+});
+
 const app = createApp(App);
 
 app.use(router);
